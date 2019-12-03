433a92188bd3dad52d8de1eb4a1f04592779f834
Andy Luo <aluo@greatschools.org>
2019-12-03 00:05:54 -0800

    solution for day 3 and started exilir solutions

---
+++ elixir/day01/part-1/answer.exs
@@ -0,0 +1,15 @@
+# file = File.read("./input.txt")
+
+sample = File.read!("./input.txt") |> String.to_charlist |> String.split |> Enum.map(&String.to_integer/1)
+IO.puts sample
+
+# IO.puts Enum.reduce(contents, fn x, acc -> acc + Integer.floor(String.to_integer(x) / 3) - 2 end) 
+
+# total_fuel = 0 
+# file.each do |mass|
+#   fuel_req = (mass.to_i / 3) - 2
+#   total_fuel += fuel_req
+# end
+
+# puts total_fuel
+# 3399947
---
+++ ruby/day03/part-2/answer.rb
@@ -0,0 +1,122 @@
+file = File.open("../part-1/input.txt")
+
+wires = []
+file.each do |wire_coords|
+  wires << wire_coords.split(',')
+end
+
+Coord = Struct.new(:x, :y)
+
+class Wire
+  NORTH = 'U'
+  SOUTH = 'D'
+  EAST = 'R'
+  WEST = 'L'
+
+  attr_reader :array_of_coords, :coords, :hash
+  attr_accessor :current_pos
+
+  def initialize(array_of_coords)
+    @array_of_coords = array_of_coords
+    @coords = []
+    @current_pos = [0,0]
+    @hash = {}
+    @steps = 0
+    process_the_coords
+  end
+
+  def process_the_coords
+    array_of_coords.each do |direction|
+      move, steps = parse_the_direction(direction)
+      if move == NORTH
+        affect_y_coords(steps)
+      elsif move == SOUTH
+        affect_y_coords(-1 * steps)
+      elsif move == EAST
+        affect_x_coords(steps)
+      elsif move == WEST
+        affect_x_coords(-1 * steps)
+      end
+    end
+  end
+
+  private
+
+  def parse_the_direction(direction)
+    move = direction[0]
+    steps = direction[1..-1]
+    [move, steps.to_i]
+  end
+
+  def affect_y_coords(value)
+    if value > 0
+      value.times do
+        @current_pos = [@current_pos.first, @current_pos.last + 1]
+        @steps +=1
+        @hash["#{current_pos.first},#{current_pos.last}"] = @steps unless @hash["#{current_pos.first},#{current_pos.last}"]
+        @coords << current_pos
+      end
+    else
+      value.abs.times do
+        @current_pos = [@current_pos.first, @current_pos.last - 1]
+        @steps +=1
+        @hash["#{current_pos.first},#{current_pos.last}"] = @steps unless @hash["#{current_pos.first},#{current_pos.last}"]
+        @coords << current_pos
+      end
+    end
+  end
+
+  def affect_x_coords(value)
+    if value > 0
+      value.times do
+        @current_pos = [@current_pos.first + 1, @current_pos.last]
+        @steps +=1
+        @hash["#{current_pos.first},#{current_pos.last}"] = @steps unless @hash["#{current_pos.first},#{current_pos.last}"]
+        @coords << current_pos
+      end
+    else
+      value.abs.times do
+        @current_pos = [@current_pos.first - 1, @current_pos.last]
+        @steps +=1
+        @hash["#{current_pos.first},#{current_pos.last}"] = @steps unless @hash["#{current_pos.first},#{current_pos.last}"]
+        @coords << current_pos
+      end
+    end
+  end
+end
+
+# puts wires
+points_array_1 = Wire.new(wires.first).coords
+points_array_2 = Wire.new(wires.last).coords
+hash_1 = Wire.new(wires.first).hash
+hash_2 = Wire.new(wires.last).hash
+crosses = points_array_1 & points_array_2
+p crosses
+# p hash_1
+# p hash_2
+min = nil
+min_pair = nil
+crosses.each do |pair|
+  if min == nil
+    min = hash_1["#{pair.first},#{pair.last}"] + hash_2["#{pair.first},#{pair.last}"]
+    min_pair = pair
+  else
+    if hash_1["#{pair.first},#{pair.last}"] + hash_2["#{pair.first},#{pair.last}"] < min
+      min = hash_1["#{pair.first},#{pair.last}"] + hash_2["#{pair.first},#{pair.last}"]
+      min_pair = pair
+    end
+  end
+end
+p min
+
+
+
+
+# crosses = points_array_1 & points_array_2
+# value = crosses.reduce(9999999) do |accum, ele|
+#   ele.first.abs + ele.last.abs < accum ? ele.first.abs + ele.last.abs : accum
+# end
+
+# p crosses.first.first.abs + crosses.first.last.abs
+
+
